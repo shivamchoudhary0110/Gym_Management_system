5cf4f6cc9f7be52b1b3cd17aa2872f8592c4309c
Samarjeet Singh <samarjeetsingh007@192.168.1.4>
2026-06-11 01:25:58 +0530

    Use Supabase pooler connection with SSL

--- GymManagementDjango/settings.py
+++ GymManagementDjango/settings.py
@@ -63,6 +63,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
